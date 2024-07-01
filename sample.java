193f7644c2111f231e5f8c5d802026e314104381
Hetzana18 <23203008@srcas.ac.in>
2024-07-01 11:55:56 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,10 @@
+class sample{
+  public static void main(String[]args)
+  {
+    System.out.ptintln("Hello");
+    System.out.ptintln("Welcome");
+    System.out.ptintln("To")
+    System.out.ptintln("DEVOPS");
+    System.out.ptintln("Lab");
+  }
+}
