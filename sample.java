fe087aaeec11363f5cc10fc8909b78bf3666cfcc
Hetzana18 <23203008@srcas.ac.in>
2024-07-01 12:22:39 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,10 +1,10 @@
 class sample{
   public static void main(String[]args)
   {
-    System.out.ptintln("Hello");
-    System.out.ptintln("Welcome");
-    System.out.ptintln("To")
-    System.out.ptintln("DEVOPS");
-    System.out.ptintln("Lab");
+    System.out.println("Hello");
+    System.out.println("Welcome");
+    System.out.println("To")
+    System.out.println("DEVOPS");
+    System.out.println("Lab");
   }
 }
